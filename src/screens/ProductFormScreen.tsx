6c5654bed9b8b9cba36ea03f53980dff4389fb1d
SILVA, H. N. <hudsonnsilva@gmail.com>
2026-04-06 15:29:12 -0300

    Update ProductFormScreen.tsx

--- src/screens/ProductFormScreen.tsx
+++ src/screens/ProductFormScreen.tsx
@@ -1,28 +1,40 @@
 import React, { useState } from 'react';
-import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  Alert,
+  FlatList,
+} from 'react-native';
+
+import { Product } from '../types/Product';
+
 
 export default function ProductFormScreen() {
   const [name, setName] = useState('');
   const [price, setPrice] = useState('');
   const [description, setDescription] = useState('');
+  const [products, setProducts] = useState<Product[]>([]);
 
   function handleSave() {
     if (!name || !price) {
       Alert.alert('Erro', 'Nome e preço são obrigatórios');
       return;
     }
-
-    const product = {
+    const newProduct: Product = {
+      id: Date.now().toString(),
       name,
       price,
       description,
     };
 
-    console.log('Produto cadastrado:', product);
+    setProducts((prev) => [...prev, newProduct]);
 
     Alert.alert('Sucesso', 'Produto cadastrado com sucesso!');
 
-    // Limpar formulário
     setName('');
     setPrice('');
     setDescription('');
@@ -30,7 +42,7 @@ export default function ProductFormScreen() {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Cadastro de Produto</Text>
+      <Text style={styles.title}>Cadastro de Pedidos</Text>
 
       <TextInput
         placeholder="Nome do produto"
@@ -55,6 +67,21 @@ export default function ProductFormScreen() {
       />
 
       <Button title="Cadastrar" onPress={handleSave} />
+
+      <Text style={styles.subtitle}>Produtos Cadastrados:</Text>
+
+      <FlatList
+        data={products}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+            <view style={styles.card}>
+                <Text style={styles.name}>{item.name}</Text>
+                <Text>R$ {item.price}</Text>
+                <Text>{item.description}</Text>
+            </view>
+        )}
+      />
+
     </View>
   );
 }
@@ -71,6 +98,22 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
+    subtitle: {
+        fontSize: 18,
+        marginVertical: 15,
+        fontWeight: 'bold',
+    },
+    card: {
+        borderWidth: 1,
+        borderColor: '#add',
+        padding: 10,
+        marginBottom: 10,
+        borderRadius: 5,
+    },
+    name: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
   input: {
     borderWidth: 1,
     borderColor: '#ccc',
